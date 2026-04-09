b0b7c4eaf2f8e7485e29169c9df649e73b0241c8
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-04-09 00:47:56 -0300

    integrate google smtp for real email notifications

--- notification-service/src/main/java/com/RuanPablo2/notification_service/listener/TicketNotificationListener.java
+++ notification-service/src/main/java/com/RuanPablo2/notification_service/listener/TicketNotificationListener.java
@@ -2,18 +2,43 @@ package com.RuanPablo2.notification_service.listener;
 
 import com.RuanPablo2.notification_service.events.TicketCreatedEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TicketNotificationListener {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
     @RabbitListener(queues = "ticket.created.queue")
     public void listenTicketCreation(TicketCreatedEvent event) {
-        System.out.println("=====================================================");
-        System.out.println("📬 [NEW NOTIFICATION] Preparing to send email...");
-        System.out.println("👤 Client: " + event.clientName());
-        System.out.println("🎫 Ticket ID: " + event.ticketId());
-        System.out.println("📝 Subject: " + event.title());
-        System.out.println("=====================================================");
+        System.out.println("📬 [NEW NOTIFICATION] Dispatching real email to the client...");
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setFrom(senderEmail);
+
+            message.setTo(senderEmail);
+
+            message.setSubject("Ticket Created: " + event.title());
+            message.setText("Hello " + event.clientName() + ",\n\n" +
+                    "Your ticket number #" + event.ticketId() + " has been successfully created in our system!\n" +
+                    "Our team will get in touch with you shortly.\n\n" +
+                    "Best regards,\nTicketFlow Team");
+
+            mailSender.send(message);
+            System.out.println("✅ Email sent successfully!");
+
+        } catch (Exception e) {
+            System.err.println("❌ Error sending email: " + e.getMessage());
+        }
     }
 }
