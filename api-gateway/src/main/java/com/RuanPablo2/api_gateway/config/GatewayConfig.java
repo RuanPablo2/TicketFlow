39c3059e479f4f63f0d3ec52be79169dcf86b332
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-04-10 19:24:05 -0300

    Implements base routing with Spring Cloud Gateway MVC

---
+++ api-gateway/src/main/java/com/RuanPablo2/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,25 @@
+package com.RuanPablo2.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> ticketRoute() {
+        System.out.println("🚦 [API GATEWAY] Registering a route using WebMVC DSL...");
+
+        return route("ticket-service-route")
+                .route(RequestPredicates.path("/api/**"), http())
+                .before(uri("http://localhost:8080"))
+                .build();
+    }
+}
