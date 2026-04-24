1ec9db631b0475a5e2810f9546c4eb4c4a8dc727
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-04-23 23:20:31 -0300

    preparing environment for production

--- api-gateway/src/main/java/com/RuanPablo2/api_gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/RuanPablo2/api_gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.RuanPablo2.api_gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RequestPredicates;
@@ -14,16 +15,17 @@ import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFu
 @Configuration
 public class GatewayConfig {
 
+    @Value("${TICKET_SERVICE_URL:http://localhost:8080}")
+    private String ticketServiceUrl;
+
     @Bean
     public RouterFunction<ServerResponse> ticketRoute() {
         System.out.println("🚦 [API GATEWAY] Registering a route using WebMVC DSL...");
 
         return route("ticket-service-route")
                 .route(RequestPredicates.path("/**"), http())
-
                 .before(rewritePath("/(?<segment>.*)", "/api/${segment}"))
-
-                .before(uri("http://localhost:8080"))
+                .before(uri(ticketServiceUrl))
                 .build();
     }
 }
